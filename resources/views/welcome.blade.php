06c4e0fd4bcc421bd46bc14555a5b5f082843d14
carlo-ddm <carloduminuco90@gmail.com>
2022-07-05 14:10:06 +0200

    redesignazione nome rotte

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Pagina pubblica
+                    Pagina Pubblica
                 </div>
             </div>
         </div>
